da5cd7003ba0341b3e9b8eaf3d07676acb87d493
Guilherme Andrade <gui-keanne@hotmail.com>
2023-10-11 11:03:55 -0300

    Resumo Vendas

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,11 +1,42 @@
-import React from "react";
 import { useData } from "../Contexts/DataContext";
 
 const Resumo = () => {
   const { data } = useData();
-  console.log(data);
 
-  return <div>Resumo</div>;
+  if (data === null) return null;
+  return (
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data
+              .filter((venda) => venda.status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data
+              .filter((venda) => venda.status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+      <div className="graficos">Gráficos</div>
+    </section>
+  );
 };
 
 export default Resumo;
