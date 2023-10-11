a8f37cabaef98967e6026e93e57876f30fc8c8f3
Guilherme Andrade <gui-keanne@hotmail.com>
2023-10-11 10:09:49 -0300

    CSSProperties

--- src/Components/DateInput.tsx
+++ src/Components/DateInput.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 
+const generalStyle: React.CSSProperties = {
+  fontSize: "1rem",
+  color: "var(--color-2)",
+  padding: "var(--gap-s) 0.75rem",
+  backgroundColor: "var(--color-4)",
+  borderRadius: "var(--gap)",
+};
+
+const labelStyle: React.CSSProperties = {
+  display: "block",
+  marginBottom: "var(--gap-s)",
+  fontWeight: "600",
+  ...generalStyle,
+};
+
+const inputStyle: React.CSSProperties = {
+  border: "none",
+  fontFamily: "monospace",
+  ...generalStyle,
+};
+
 type IDateInput = React.ComponentProps<"input"> & {
   label: string;
 };
@@ -7,8 +28,16 @@ type IDateInput = React.ComponentProps<"input"> & {
 const DateInput = ({ label, ...props }: IDateInput) => {
   return (
     <div>
-      <label htmlFor={props.id}>{label}</label>
-      <input type="date" name={props.id} id={props.id} {...props} />
+      <label style={labelStyle} htmlFor={props.id}>
+        {label}
+      </label>
+      <input
+        style={inputStyle}
+        type="date"
+        name={props.id}
+        id={props.id}
+        {...props}
+      />
     </div>
   );
 };
